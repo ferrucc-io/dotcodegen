135e46a4517d6bbc6be2761cafe6afc4feb56bfc
Ferruccio Balestreri <ferruccio.balestreri@gmail.com>
2024-03-10 14:21:03 +0100

    Add SHA checksum to gem

--- homebrew-tap/Formula/dotcodegen.rb
+++ homebrew-tap/Formula/dotcodegen.rb
@@ -3,7 +3,7 @@ class Dotcodegen < Formula
   homepage "https://github.com/ferrucc-io/dotcodegen"
   url "https://github.com/ferrucc-io/dotcodegen/archive/main.tar.gz"
   version "0.1.0"
-  sha256 "checksum"
+  sha256 "e2282bde80c5a374201abc67d1bbad24647cd652b2de9b18273fa1aa8bdce181"
 
   depends_on "ruby@3.3"
 
